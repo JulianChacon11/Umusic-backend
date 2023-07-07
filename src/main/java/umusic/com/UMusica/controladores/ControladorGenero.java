bae4f9f144bd9d3f8ee0901a012956f1ae7b73e4
JulianChacon11 <82472712+JulianChacon11@users.noreply.github.com>
2023-07-06 20:27:58 -0500

    Update controlador genero
    able crossorigin from port 8080

--- src/main/java/umusic/com/UMusica/controladores/ControladorGenero.java
+++ src/main/java/umusic/com/UMusica/controladores/ControladorGenero.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import umusic.com.UMusica.servicios.IGeneroService;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/api/generos")
 public class ControladorGenero {
     
